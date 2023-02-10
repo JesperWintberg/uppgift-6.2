1bd2b86603d3a79c62eb66ec33a06f52e68e2afb
JesperWintberg <jesper.wintberg@gmail.com>
2023-02-10 10:11:17 +0100

    Add project files.

---
+++ uppgift 6.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            störst(7, 6);
+        }
+
+        static void störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                Console.WriteLine($"{tal1} är det största talet");
+            }
+
+            else if(tal1 < tal2)
+            {
+                Console.WriteLine($"{tal2} är det största talet");
+            }
+
+            else
+            {
+                Console.WriteLine($"{tal1} och {tal2} är lika stora");
+            }
+        }
+    }
+}
+
